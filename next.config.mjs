d029fe7fda879d34f2fe7090d34c170926443f48
Satyam Khatiwada <99595892+satyamkhatiwada@users.noreply.github.com>
2025-12-31 21:36:29 +0545

    updated remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,17 @@
 const nextConfig = {
   output: "standalone",
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
         hostname: "d2th3dc33uqqn2.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
       },
     ],
   },
